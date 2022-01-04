af69292b66a70dd806e361308188af8be99cfc40
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2022-01-04 22:05:14 +0900

    daily commit

---
+++ 220104.py
@@ -0,0 +1,21 @@
+# baekjoon 2750
+# 수 정렬하기 B1
+
+import sys
+
+sys.stdin = open('220104.txt', 'r')
+
+arr = []
+n = int(input())
+for i in range(n):
+    arr.append(int(input()))
+
+print(arr)
+for i in range(n):
+    for j in range(n-i-1):
+        if(arr[j] > arr[j+1]):
+            arr[j+1], arr[j] = arr[j], arr[j+1]
+print(arr)
+
+for i in arr:
+    print(i)
